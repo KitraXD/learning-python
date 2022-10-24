d75e7e82e393051ef1b24349f5048f09ca6b1e25
Kitra <116291878+KitraXD@users.noreply.github.com>
2022-10-24 11:43:10 +0800

    Update python 1.py

--- python 1.py
+++ python 1.py
@@ -1,6 +1,20 @@
-print("Hello World")
-oof = 0
-if oof == 1:
-  print("oof indeed")
-else:
-  print("no oof")
+loop = 1
+while loop < 2:
+    text_enter = input("Enter your text: ")
+    if text_enter == "end":
+        loop += 1
+    
+    if text_enter == "hello":
+        print("hello there!")
+    
+    if text_enter == "hi":
+        print("hi there!")
+    
+    if text_enter == "hey":
+        print("hey there!")
+
+    if loop == 2:
+        break
+    
+    else:
+        print("unsure...")
